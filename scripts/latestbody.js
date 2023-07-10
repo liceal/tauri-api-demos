fd38399845d49309a230fc56e8c9164c93df4770
linxianao <linxianao@jmcompany.cn>
2023-07-10 16:43:22 +0800

    try: 更改附件读取

--- scripts/latestbody.js
+++ scripts/latestbody.js
@@ -1,6 +1,28 @@
-import updatelog from "./updatelog.mjs";
-if(process.argv[2]){
-  return updatelog(process.argv[2])
-}else{
-  return 'See the assets to download this version and install.'
+const fs = require('fs');
+const path = require('path');
+
+const UPDATE_LOG = 'UPDATE_LOG.md';
+
+const file = path.join(process.cwd(), UPDATE_LOG);
+if (!fs.existsSync(file)) {
+  console.log('Could not found UPDATE_LOG.md');
+  process.exit(1);
+}
+const log = fs.readFileSync(file, { encoding: 'utf8' });
+
+
+// 使用正则表达式匹配第一个版本号和内容
+const regex = /##\s+(v\d+\.\d+\.\d+)\s+([\s\S]*?)(?=##\s+v\d+\.\d+\.\d+|$)/;
+const match = regex.exec(log);
+if (match) {
+  const version = match[1];
+  const content = match[2].trim();
+  // console.log(`Version: ${version}`);
+  // console.log(`Content: ${content}`);
+  console.log(content);
+  return content
+
+} else {
+  console.log("No matching version found.");
+  return ''
 }
